6da94d7e777c883cb4b479c5fa6ef1c384c440f6
Gusto Bacvinka <augustinas.bacvinka@gmail.com>
2023-04-11 23:47:53 +0300

    Remove unused plugins

--- lua/inka/packer.lua
+++ lua/inka/packer.lua
@@ -4,43 +4,31 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
 
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        -- or                            , branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		requires = { { 'nvim-lua/plenary.nvim' } }
+	}
 
-    use({ 'rose-pine/neovim', as = 'rose-pine' })
-    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-    use('nvim-treesitter/playground')
-    use('nvim-tree/nvim-tree.lua')
+	use({ 'rose-pine/neovim', as = 'rose-pine' })
+	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+	use('nvim-tree/nvim-tree.lua')
 
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        requires = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' }, -- Required
-            {
-                -- Optional
-                'williamboman/mason.nvim',
-                run = function()
-                    pcall(vim.cmd, 'MasonUpdate')
-                end,
-            },
-            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		requires = {
+			-- LSP Support
+			{ 'neovim/nvim-lspconfig' }, -- Required
 
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },     -- Required
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'hrsh7th/vim-vsnip' },
-            { 'hrsh7th/cmp-vsnip',                branch = 'main' },
-            { 'L3MON4D3/LuaSnip' }, -- Required
-
-            { 'rust-lang/rust' }
-        }
-    }
+			-- Autocompletion
+			{ 'hrsh7th/nvim-cmp' }, -- Required
+			{ 'hrsh7th/cmp-nvim-lsp' }, -- Required
+			{ 'hrsh7th/vim-vsnip' },
+			{ 'hrsh7th/cmp-vsnip',    branch = 'main' },
+			{ 'L3MON4D3/LuaSnip' }, -- Required
+		}
+	}
 end)
