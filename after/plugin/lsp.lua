de389009e704daf6d8c57ba090efd61e53b600fc
Gusto <bacvinka@gmail.com>
2024-01-24 22:23:24 +0200

    Set lua formatting option

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -6,80 +6,88 @@ local lspconfig = require 'lspconfig'
 local util = require 'lspconfig/util'
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
--- (Optional) Configure lua language server for neovim
-lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
+local lua_opts = lsp.nvim_lua_ls()
+lua_opts["format"] = {
+    enable = true,
+    defaultConfig = {
+        indent_style = "space",
+        indent_size = "4",
+    }
+}
+
+lspconfig.lua_ls.setup(lua_opts)
 
 lsp.set_preferences({
-	suggest_lsp_servers = false,
-	sign_icons = {
-		error = 'E',
-		warn = 'W',
-		hint = 'H',
-		info = 'I'
-	}
+    suggest_lsp_servers = false,
+    sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
 })
 
-local on_attach = function(client, bufnr)
-	lsp.default_keymaps({ buffer = bufnr })
-	local opts = { buffer = bufnr, remap = false }
+local on_attach = function(_, bufnr)
+    lsp.default_keymaps({ buffer = bufnr })
+    local opts = { buffer = bufnr, remap = false }
 
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, opts)
-	vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
-	vim.keymap.set('n', 'gi', function() vim.lsp.buf.implementation() end, opts)
-	vim.keymap.set('n', '<C-k>', function() vim.lsp.buf.signature_help() end, opts)
-	vim.keymap.set('n', '<space>D', function() vim.lsp.buf.type_definition() end, opts)
-	vim.keymap.set('n', '<space>r', function() vim.lsp.buf.rename() end, opts)
-	vim.keymap.set('n', '<space>a', function() vim.lsp.buf.code_action() end, opts)
-	vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, opts)
-	vim.keymap.set('n', '<space>e', function() vim.diagnostic.open_float() end, opts)
-	vim.keymap.set('n', '[d', function() vim.diagnostic.goto_prev() end, opts)
-	vim.keymap.set('n', ']d', function() vim.diagnostic.goto_next() end, opts)
-	vim.keymap.set('n', '<space>q', function() vim.diagnostic.setloclist() end, opts)
-	vim.keymap.set("n", "<space>f", function() vim.lsp.buf.format() end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, opts)
+    vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set('n', 'gi', function() vim.lsp.buf.implementation() end, opts)
+    vim.keymap.set('n', '<C-k>', function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set('n', '<space>D', function() vim.lsp.buf.type_definition() end, opts)
+    vim.keymap.set('n', '<space>r', function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set('n', '<space>a', function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set('n', '<space>e', function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set('n', '[d', function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set('n', ']d', function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set('n', '<space>q', function() vim.diagnostic.setloclist() end, opts)
+    vim.keymap.set("n", "<space>f", function() vim.lsp.buf.format() end, opts)
 end
 
 lspconfig.rust_analyzer.setup {
-	on_attach = on_attach,
-	flags = {
-		debounce_text_changes = 150,
-	},
-	settings = {
-		["rust-analyzer"] = {
-			cargo = {
-				allFeatures = true,
-			},
-			completion = {
-				postfix = {
-					enable = false,
-				},
-			},
-			checkOnSave = {
-				command = "clippy"
-			},
-		},
-	},
-	capabilities = capabilities,
+    on_attach = on_attach,
+    flags = {
+        debounce_text_changes = 150,
+    },
+    settings = {
+        ["rust-analyzer"] = {
+            cargo = {
+                allFeatures = true,
+            },
+            completion = {
+                postfix = {
+                    enable = false,
+                },
+            },
+            checkOnSave = {
+                command = "clippy"
+            },
+        },
+    },
+    capabilities = capabilities,
 }
 
 lspconfig.gopls.setup {
-	on_attach = on_attach,
-	cmd = { "gopls", "serve" },
-	filetypes = { "go", "gomod" },
-	root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-	settings = {
-		gopls = {
-			analyses = {
-				unusedparams = true,
-			},
-			staticcheck = true,
-		},
-	},
+    on_attach = on_attach,
+    cmd = { "gopls", "serve" },
+    filetypes = { "go", "gomod" },
+    root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+    settings = {
+        gopls = {
+            analyses = {
+                unusedparams = true,
+            },
+            staticcheck = true,
+        },
+    },
 }
 
 lspconfig.zls.setup {
-	on_attach = on_attach,
+    on_attach = on_attach,
 }
 
 lsp.on_attach(on_attach)
@@ -87,42 +95,42 @@ lsp.setup()
 
 local cmp = require 'cmp'
 cmp.setup({
-	-- Enable LSP snippets
-	snippet = {
-		expand = function(args)
-			vim.fn["vsnip#anonymous"](args.body)
-		end,
-	},
-	mapping = {
-		['<M-k>'] = cmp.mapping.select_prev_item(),
-		['<M-j>'] = cmp.mapping.select_next_item(),
-		-- Add tab support
-		['<S-Tab>'] = cmp.mapping.select_prev_item(),
-		['<Tab>'] = cmp.mapping.select_next_item(),
-		['<M-,>'] = cmp.mapping.scroll_docs(-4),
-		['<M-m>'] = cmp.mapping.scroll_docs(4),
-		['<M-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.close(),
-		['<CR>'] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Insert,
-			select = true,
-		})
-	},
-	-- Installed sources
-	sources = {
-		{ name = 'nvim_lsp' },
-		{ name = 'vsnip' },
-		{ name = 'path' },
-		{ name = 'buffer' },
-	},
-	experimental = {
-		ghost_text = true,
-	},
+    -- Enable LSP snippets
+    snippet = {
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+        end,
+    },
+    mapping = {
+        ['<M-k>'] = cmp.mapping.select_prev_item(),
+        ['<M-j>'] = cmp.mapping.select_next_item(),
+        -- Add tab support
+        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+        ['<Tab>'] = cmp.mapping.select_next_item(),
+        ['<M-,>'] = cmp.mapping.scroll_docs(-4),
+        ['<M-m>'] = cmp.mapping.scroll_docs(4),
+        ['<M-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.close(),
+        ['<CR>'] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true,
+        })
+    },
+    -- Installed sources
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'vsnip' },
+        { name = 'path' },
+        { name = 'buffer' },
+    },
+    experimental = {
+        ghost_text = true,
+    },
 })
 
 -- Enable completing paths in :
 cmp.setup.cmdline(':', {
-	sources = cmp.config.sources({
-		{ name = 'path' }
-	})
+    sources = cmp.config.sources({
+        { name = 'path' }
+    })
 })
